aab5157141b577d54378fed1f44e40326d517134
Anton Kupranis <bdynamite@yandex.ru>
2017-02-12 15:17:47 +0300

    Первая попытка

--- bars.py
+++ bars.py
@@ -1,21 +1,42 @@
 import json
+import math
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r', encoding='windows-1251') as file:
+        data = json.load(file)
+        return data
 
 
 def get_biggest_bar(data):
-    pass
+    print((data[-1]['Name'], data[-1]['SeatsCount']))
 
 
 def get_smallest_bar(data):
-    pass
+    print((data[0]['Name'], data[0]['SeatsCount']))
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    closest_bar = get_bar_params(data[0], longitude, latitude)
+    for bar in data:
+        bar_params = get_bar_params(bar, longitude, latitude)
+        if bar_params[1] < closest_bar[1]:
+            closest_bar = bar_params
+    print(closest_bar)
+
+
+def get_bar_params(bar, longitude, latitude):
+    name = bar['Name']
+    coordinates = (bar['geoData']['coordinates'])
+    distance = math.sqrt(((float(coordinates[0]) - float(longitude)) ** 2 + (float(coordinates[1]) - float(latitude)) ** 2))
+    return (name, distance)
 
 
 if __name__ == '__main__':
-    pass
+    bars = load_data('data-2897-2016-11-23.json')
+    sorted_bars = sorted(bars, key=lambda x: x['SeatsCount'])
+    get_biggest_bar(sorted_bars)
+    get_smallest_bar(sorted_bars)
+    get_closest_bar(bars, input('input your longitude: '), input('input your latitude: '))
+
+
