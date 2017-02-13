d72f265d2381b0d70c2bb43c806860dd262902b9
Anton Kupranis <bdynamite@yandex.ru>
2017-02-13 16:18:28 +0300

    Четвертая попытка

--- bars.py
+++ bars.py
@@ -7,12 +7,12 @@ import chardet
 def load_data(my_json_path):
     if not os.path.exists(my_json_path):
         raise Exception("file not found!")
-    encoding = code_detecter(filepath)
+    encoding = get_encoding(filepath)
     with open(my_json_path, 'r', encoding=encoding) as file:
         return json.load(file)
 
 
-def code_detecter(path_to_file_text):
+def get_encoding(path_to_file_text):
     with open(path_to_file_text, 'rb') as source:
         lines = source.read()
         result = chardet.detect(lines)
@@ -30,7 +30,29 @@ def get_smallest_bar(data):
     print('The smallest bar is "{}" with {} seats'.format(data[0]['Name'], data[0]['SeatsCount']))
 
 
-def get_closest_bar(data):
+def get_closest_bar(data, my_longitude, my_latitude):
+    closest_bar = get_bar_params(data[0], my_longitude, my_latitude)
+    for bar in data:
+        bar_params = get_bar_params(bar, my_longitude, my_latitude)
+        if bar_params[1] < closest_bar[1]:
+            closest_bar = bar_params
+    print('The closest bar is "{}"'.format(closest_bar[0]))
+
+
+def get_bar_params(bar, my_longitude, my_latitude):
+    bar_name = bar['Name']
+    coordinates = (bar['geoData']['coordinates'])
+    distance = math.sqrt(((float(coordinates[0]) - float(my_longitude)) ** 2
+                          + (float(coordinates[1]) - float(my_latitude)) ** 2))
+    return bar_name, distance
+
+
+if __name__ == '__main__':
+    filepath = input('input data path: ')
+    bars = load_data(filepath)
+    sorted_bars = sorted(bars, key=lambda x: x['SeatsCount'])
+    get_biggest_bar(sorted_bars)
+    get_smallest_bar(sorted_bars)
 
     try:
         longitude = float(input('input your longitude: '))
@@ -46,26 +68,4 @@ def get_closest_bar(data):
     if latitude is None:
         print('wtf, dude?')
 
-    closest_bar = get_bar_params(data[0], longitude, latitude)
-    for bar in data:
-        bar_params = get_bar_params(bar, longitude, latitude)
-        if bar_params[1] < closest_bar[1]:
-            closest_bar = bar_params
-    print('The closest bar is "{}"'.format(closest_bar[0]))
-
-
-def get_bar_params(bar, longitude, latitude):
-    name = bar['Name']
-    coordinates = (bar['geoData']['coordinates'])
-    distance = math.sqrt(((float(coordinates[0]) - float(longitude)) ** 2
-                          + (float(coordinates[1]) - float(latitude)) ** 2))
-    return name, distance
-
-
-if __name__ == '__main__':
-    filepath = input('input data path: ')
-    bars = load_data(filepath)
-    sorted_bars = sorted(bars, key=lambda x: x['SeatsCount'])
-    get_biggest_bar(sorted_bars)
-    get_smallest_bar(sorted_bars)
-    get_closest_bar(bars)
+    get_closest_bar(bars, longitude, latitude)
